f1a7594f19e5dd0bd6733234272c203ab04ed440
Derek Chen <derek.g.chen@gmail.com>
2018-06-03 15:21:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ let ageIsValid = Number.isInteger(currentAge);
 
 let randomNumber = Math.random() * 20;
 let randomInteger = Math.floor(randomNumber) ;
-let randomUserID = randomNumber + newID;
+let randomUserID = randomNumber + 1000000000;

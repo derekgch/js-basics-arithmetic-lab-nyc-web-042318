7cdb067e730d70d7eeb0258683b71ba2f0a2cc39
Derek Chen <derek.g.chen@gmail.com>
2018-06-03 15:11:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ You should NOT try to redefine any of the above variables.
 
 let newID = oldID + 1000000000;
 
-let ageIsValid = true if typeof(currentAge) == integer
+let ageIsValid = true if typeof(currentAge) == integer;
